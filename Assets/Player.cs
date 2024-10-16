845213de3fa6f77dc8c305df22b5147ae80948e4
Maico Borges <maicoqb@gmail.com>
2024-10-16 14:07:54 -0300

    player: create player movement on platform

---
+++ Assets/Player.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 5f;
+    public float jumpForce = 10f;
+
+    private Rigidbody2D rb;
+
+    private bool isJumping = false;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float movement = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(movement * speed, rb.velocity.y);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+    }
+
+    void Jump()
+    {
+        if (isJumping) return;
+
+        isJumping = true;
+
+        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            isJumping = false;
+        }
+    }
+}
